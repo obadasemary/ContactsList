a6e1dbe54042643464d358f3a822fa9a6fb2891b
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-05-29 18:21:53 +0300

    1.0
    Add CoreDataStackManager
    Add CreateNewAccount

--- ContactsList/ViewController.swift
+++ ContactsList/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var context: NSManagedObjectContext?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        context = CoreDataStackManager.sharedInstacne().managedObjectContext
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginAction(sender: AnyObject) {
+        
     }
-
-
+    
+    @IBAction func createAccountAction(sender: AnyObject) {
+        
+        alertDialog()
+    }
+    
+    func alertDialog() {
+        
+        let alert = UIAlertController(title: "Create New Account", message: "Enter Username and Password", preferredStyle: .Alert)
+        
+        // create btn
+        let create = UIAlertAction(title: "Save", style: .Default, handler: { (UIAlertAction) in
+            
+            let usernameField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+            
+            if (usernameField.text != "" && passwordField.text != "") {
+                self.createUserAccount(usernameField.text!, password: passwordField.text!)
+            } else {
+                print("Error must enter username or password")
+            }
+        })
+        
+        // cancel btn
+        let cancel = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+        
+        // textfields
+        alert.addTextFieldWithConfigurationHandler { (usernameText) in
+            
+            usernameText.placeholder = "username"
+        }
+        alert.addTextFieldWithConfigurationHandler { (passwordText) in
+            
+            passwordText.placeholder = "password"
+        }
+        
+        alert.addAction(create)
+        alert.addAction(cancel)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func createUserAccount(username: String, password: String) {
+        
+        // new record - CREATE
+        let newUser = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: context!)
+        newUser.setValue(username, forKey: "username")
+        newUser.setValue(password, forKey: "password")
+        
+        // Save
+        do {
+            try context?.save()
+            print("New Account for \(username) successfully created")
+        } catch {
+            print("Error Saving to Core Data")
+        }
+    }
 }
 
